6f70495542d111052cc8fe9c208dea546457d81f
ymp-a <hiyakashi3@gmail.com>
2020-08-13 14:27:55 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'
+# get 'posts/new', to: 'posts#new'←これは削除する
   post 'posts', to: 'posts#create'
 end
