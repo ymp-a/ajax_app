4ee1736005ab1c9f16e38a65e5818a8cbed4a9d6
ymp-a <hiyakashi3@gmail.com>
2020-08-14 12:27:26 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,13 @@ class PostsController < ApplicationController
   end
 
   def create
+
     Post.create(content: params[:content])
     redirect_to action: :index
   end
 
   def checked
+
     post = Post.find(params[:id])
     if post.checked then
       post.update(checked: false)
